c69ea3f3efcfdbbac13d29996a72d241bcaa4175
Evan Liang <evan_liang@163.com>
2015-02-28 13:50:28 +0800

    add jars

--- du.tools.main/src/main/java/du/tools/main/mq/MHelper.java
+++
@@ -1,7 +0,0 @@
-package du.tools.main.mq;
-
-public interface MHelper {
-    QHelper getQHelper(String queueName) throws Exception;
-
-    void close() throws Exception;
-}
--- du.tools.main/src/main/java/du/tools/main/widgets/unix/LFileModel.java
+++
@@ -1,8 +0,0 @@
-package du.tools.main.widgets.unix;
-
-public class LFileModel extends DFileModel<LFileNode> {
-
-    public LFileModel(LFileNode root) {
-        super(root);
-    }
-}
