7ea823e20ebf2bfcf30a0e0b99a7ade72e615e81
EvanLiang <EvanLiang@users.noreply.github.com>
2015-03-02 14:44:52 +0800

    Add bat script for sftp launch

---
+++ du.tools.sftp/src/main/resources/SFTP-Tool.bat
@@ -0,0 +1,16 @@
+@echo off
+@echo off&setlocal enabledelayedexpansion
+
+set JAVA_HOME="C:\Program Files (x86)\Java\jdk1.7.0_06"
+set CLASSPATH=.;%JAVA_HOME%\jre\lib
+set APP_HOME=%cd%
+echo %APP_HOME%
+
+for %%i in (dir /b/s %APP_HOME%\*.jar) do set CL_PA=!CL_PA!;%%i
+for %%i in (dir /b/s %APP_HOME%\lib\*.jar) do set CL_PA=!CL_PA!;%%i
+echo %CL_PA%
+
+set JVM_ARGS="-Xms512M"
+@REM set JVM_ARGS="%JVM_ARGS% -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005"
+
+START "Launching..." %JAVA_HOME%/bin/javaw.exe %JVM_ARGS% -classpath "%CL_PA%" du.tools.sftp.main.App
